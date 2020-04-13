877fd72d5036e4658dc500725ad6de18fb0e6ab1
zaphodikus <zaphodikus@hotmail.com>
2020-04-13 12:19:23 +0100

    Added a compact listing format; generate outputs

--- export_books.py
+++ export_books.py
@@ -15,19 +15,38 @@ class ImportPratchett(object):
                 result.append(row)
         return result
 
-class ExportHTML(object):
-    def __init__(self, books):
+class TextSaver(object):
+    @staticmethod
+    def save_text(text, filename):
+        with open(filename, "w") as f:
+            f.write(text)
+
+
+class ExportHTML(TextSaver):
+    def __init__(self, books= None):
         self._books = books
 
     def getvalue(self):
         return self.export_html(self._books, headings=["Title","Link","Hash","Book type"])
 
     @staticmethod
-    def save_html(text, filename):
-        with open(filename, "w") as f:
-            f.write(text)
+    def _rowrender(row, indent =2):
+        """ By default use 4 headings in the table
+        """
+        h = "{}<tr>".format(' ' * indent)
+        for item in row:
+            h += "<td>{}</td>".format(item)
+        h += "\n{}</tr>".format(' ' * indent)
+        return h
 
-    def export_html(self, books, headings):
+    @staticmethod
+    def _compactrender(row):
+        """ Tiny table with title and URL combined"""
+        h = "<tr><td><a href=""{}"">{}</a></td></tr>\n".format(row[1], row[0])
+        return h
+
+
+    def export_html(self, books, headings, rowrender=_rowrender):
         html = """<html><table border="1">
 """
         def makeheading(row, indent=2):
@@ -37,25 +56,86 @@ class ExportHTML(object):
             h+= "\n{}</tr>".format(' '*indent)
             return h
 
-        def makerow(row, indent=2):
-            h=  "{}<tr>".format(' '*indent)
-            for item in row:
-                h+= "<td>{}</td>".format(item)
-            h+= "\n{}</tr>".format(' '*indent)
-            return h
+        def makerow(row, rowrender, indent=2):
+            if callable(rowrender):
+                return rowrender(row)
+            else:
+                return rowrender.__func__(row)
+
 
         head = None
         for row in books:
             if head:
-                html += makerow(row)
+                html += makerow(row, rowrender)
             else:
                 head = makeheading(headings)
                 html += head
         html += "</table></html>"
         return html
 
-class ExportMarkdown(object):
-    pass
+
+class ExportMarkdown(TextSaver):
+    def __init__(self, books):
+        self._books = books
+
+    def getvalue(self):
+        return self.export_mark_down(self._books, headings=["Title", "Link", "Hash", "Book type"])
+
+    @staticmethod
+    def save_text(text, filename):
+        with open(filename, "w") as f:
+            f.write(text)
+
+
+    def makerow(self, row):
+        r = "|"
+        for item in row:
+            r += " {} |".format(item)
+        r += "\n"
+        return r
+
+    def makeheading(self, row):
+        h = "\n"
+        h += self.makerow(row)
+        for item in row:
+            h += "| --- "
+        h += "|\n"
+        return h
+
+    def export_mark_down(self, books, headings):
+        mark_down = """##H2 Books
+    """
+
+        mark_down = None
+        for row in books:
+            if mark_down:
+                mark_down += self.makerow(row)
+            else:
+                mark_down = self.makeheading(headings)
+        return mark_down
+
+class ExportCompactMarkdown(ExportMarkdown):
+    def __init__(self):
+        self._books = None
+
+    def getvalue(self):
+        pass # do not call!
+
+    def makerow(self,row):
+        """ All we do is redefine the row helpers
+        """
+        r = "| [{}]({}) |\n".format(row[0], row[1])
+        return r
+
+    def makeheading(self, head):
+        h = "\n|"
+        for item in head:
+            h += " {} |".format(item)
+        h += "\n"
+        for item in head:
+            h += "| --- "
+        h += "|\n"
+        return h
 
 class MergeMarkDown(object):
     pass
--- tests.py
+++ tests.py
@@ -1,11 +1,13 @@
-# note: running this test suite successfully will generate the actual target outputs at the same time
+# Note: running this test suite successfully will generate the actual target outputs at the same time
+# If the tests pass and a manual check that readme.md looks good, commit and push
 import unittest
-from export_books import ImportPratchett, ExportHTML, ExportMarkdown, MergeMarkDown
+from export_books import ImportPratchett, ExportHTML, ExportMarkdown, MergeMarkDown, ExportCompactMarkdown
 
 data_file = 'data.csv'
 html_file = 'index.html'
 markdown_file = 'index.md'
-merge_file = 'readme.md'
+template_file = 'template.md'
+merge_file = 'README.md'
 
 class ImporterTests(unittest.TestCase):
     def test_import_csv(self):
@@ -22,14 +24,33 @@ class ExporterTests(unittest.TestCase):
         csv = imp.import_csv(data_file)
         html = ExportHTML(csv).getvalue()
         assert len(html)
-        ExportHTML.save_html(ExportHTML(csv).getvalue(), html_file)
+        # export again, but the short form with just links
+        html = ExportHTML()
+        text = html.export_html(csv, ['Title'], ExportHTML._compactrender)
+        # save to file
+        ExportHTML.save_text(text, html_file)
 
     def test_export_markdown(self):
-        self.assertEqual(True, False)
+        imp = ImportPratchett()
+        csv = imp.import_csv(data_file)
+        md = ExportMarkdown(csv).getvalue()
+        assert len(md)
+        md = ExportCompactMarkdown()
+        text = md.export_mark_down(csv, ['Title'])
+        # save
+        md.save_text(text, markdown_file)
 
 class MergeTests(unittest.TestCase):
     def test_merge(self):
-        self.assertEqual(True, False)
+        imp = ImportPratchett()
+        csv = imp.import_csv(data_file)
+        md = ExportCompactMarkdown()
+        text = md.export_mark_down(csv, ['Title'])
+        with open(template_file) as f:
+            template = f.readlines()
+        transformed = "".join(template).replace("<TABLE>", text)
+        generated = transformed.replace("<GENERATED>", "Note: This file was generated from {}".format(template_file))
+        md.save_text(generated, merge_file)  # readme.md
 
 
 if __name__ == '__main__':
